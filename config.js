caa520604f4b69c429f67625bf1a5c488666b450
ANZAD <anzadibayana@gmail.com>
2025-06-12 06:41:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY PRINCE XMD*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "false ",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
@@ -23,13 +23,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/u0cfgo.jpeg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/0kwukb.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "PRINCE XMD",
+BOT_NAME: process.env.BOT_NAME || "KINGDOM XMD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "PRINCE XMD",
+STICKER_NAME: process.env.STICKER_NAME || "KINGDOM XMD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,15 +37,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255623672733",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255654667145",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "PRINCE-TECH",
+OWNER_NAME: process.env.OWNER_NAME || "KINGDOM-TECH",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*POWERED BY PRINCE TECH*",
+DESCRIPTION: process.env.DESCRIPTION || "*POWERED BY KINGDOM TECH*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/15vem5.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/0kwukb.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> PRINCE XMD-𝐁𝐎𝐓 IS ALIVE 🇹🇿",
+LIVE_MSG: process.env.LIVE_MSG || "> KINGDOM XMD-𝐁𝐎𝐓 IS ALIVE 🇹🇿",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -53,7 +53,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
@@ -71,12 +71,12 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255623672733",
+DEV: process.env.DEV || "255654667145",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
